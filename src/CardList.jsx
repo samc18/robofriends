05a1b9b0e3a6145dc90df57b5da938a3128e01fd
Samuel <93607710+samc18@users.noreply.github.com>
2023-03-01 21:19:30 -0500

    feat: add card list component

---
+++ src/CardList.jsx
@@ -0,0 +1,23 @@
+import Card from './Card'
+
+const CardList = ({ robots }) => {
+    return (
+        <div>
+            {
+                robots.map((user, i) => {
+                        return (
+                            <Card
+                                key={i}
+                                id={user.id}
+                                name={user.name}
+                                email={user.email}
+                            />
+                        )
+                    }
+                )
+            }
+        </div>
+    )
+}
+
+export default CardList
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import 'tachyons'
+import CardList from './CardList'
+import { robots } from './robots'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <CardList robots={robots} />
+  </div>
 )
