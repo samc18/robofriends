07c7fb97bb93e3b99d89907177b4496166e1a383
Samuel <93607710+samc18@users.noreply.github.com>
2023-03-03 16:02:16 -0500

    refactor: improve files structure and display

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react'
-import CardList from './CardList'
-import SearchBox from './SearchBox'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll'
 import './App.css'
 
 class App extends Component {
@@ -25,20 +26,21 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const { robots, searchfield } = this.state
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
-        if (this.state.robots.length === 0) {
-            return <h1>Loading</h1>
-        } else {
-            return (
-                <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
+        return !robots.length ?
+        <h1>Loading</h1> :
+        (
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
                     <CardList robots={filteredRobots} />
-                </div>
-            )
-        }
+                </Scroll>
+            </div>
+        )
     }
 }
 
