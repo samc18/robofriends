1ce0e15e4785eabb16ab48350969de84482454a8
Samuel <93607710+samc18@users.noreply.github.com>
2023-03-03 16:09:46 -0500

    style: update scroll styles

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -1,6 +1,6 @@
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '5px solid black', height: '700px'}}>
+        <div style={{overflowY: 'scroll', height: '680px'}}>
             {props.children}
         </div>
     )
